aff2b52ce65d3c8af78b06d4cc8a3fb9dd623b16
zh.nedyalkow <zh.nedyalkow@gmail.com>
2018-11-04 12:35:48 -0800

    Add /GET todos route

--- server/server.js
+++ server/server.js
@@ -1,36 +1,54 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+    mongoose
+} = require('./db/mongoose');
+var {
+    Todo
+} = require('./models/todo');
+var {
+    User
+} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
+    var todo = new Todo({
+        text: req.body.text
+    });
 
-  console.log(req.body);
+    console.log(req.body);
 
-  console.log(`Req.body.text = ${req.body.text}`);
+    console.log(`Req.body.text = ${req.body.text}`);
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    console.log(e);
-    res.status(400).send(e);
-  });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        console.log(e);
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+    console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = {
+    app
+};
 
 
 // get from client JSON convert it into obj and attached it to req
