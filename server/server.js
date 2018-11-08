c46574eff23d8e1f7d153271079178dcab06d9c7
zh.nedyalkow <zh.nedyalkow@gmail.com>
2018-11-07 21:27:40 -0800

    Add test cases for /GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 var {
     mongoose
@@ -40,6 +41,60 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+
+// GET /todos/121345
+// params => {key:value}
+// key value pair => url
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    console.log(id);
+
+    // Valid id using isValid
+    if (!ObjectId.isValid(id)) {
+        console.log('Id is not valid');
+        res.status(404).send({});
+    } 
+
+    // if not valid return 404 - send back empty body
+
+    Todo.findById({
+        _id: id
+    }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({
+            todo
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+
+    
+    // Todo.findById({
+    //     _id: id
+    // }).then((todo) => {
+    //     res.send({
+    //         todos
+    //     });
+    // }).catch((e) => {
+    //     res.status(404).send({});
+    // });
+
+    // findById()
+    // success
+    // err 
+       // 400 - request was not valid
+       // send back nothing
+       // if todo - send it back
+       // if not todo - send back 404 empty body
 });
 
 app.listen(3000, () => {
